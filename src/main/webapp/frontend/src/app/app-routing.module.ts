43fca6aa9089a9ffc2e7e652c49443f0229edce0
HaykazPaytyan <laguna41!>
2022-03-22 12:26:08 +0400

    Project commit

--- src/main/webapp/frontend/src/app/app-routing.module.ts
+++ src/main/webapp/frontend/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {IssuerComponent} from "./issuer/issuer.component";
+import {SubissuerComponent} from "./subissuer/subissuer.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IssuerComponent},
+  {path: 'subissuer', component: SubissuerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routesList = [IssuerComponent,SubissuerComponent];
--- src/main/webapp/frontend/src/app/app.module.ts
+++ src/main/webapp/frontend/src/app/app.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import {ReactiveFormsModule} from "@angular/forms";
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routesList } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IssuerComponent } from './issuer/issuer.component';
 import { MainComponent } from './layouts/main/main.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    IssuerComponent,
     MainComponent,
     NavbarComponent
+
   ],
   imports: [
     BrowserModule,
