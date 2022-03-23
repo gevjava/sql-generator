1e84a22aa55b45d34baaf59bc555d732bd8bca92
Gevorg Aghajanyan <gevorga@energizeglobal.com>
2022-03-23 13:13:49 +0400

    Revert "Project commit"

--- src/main/webapp/frontend/src/app/app-routing.module.ts
+++ src/main/webapp/frontend/src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {IssuerComponent} from "./issuer/issuer.component";
-import {SubissuerComponent} from "./subissuer/subissuer.component";
 
-const routes: Routes = [
-  {path: '', component: IssuerComponent},
-  {path: 'subissuer', component: SubissuerComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routesList = [IssuerComponent,SubissuerComponent];
