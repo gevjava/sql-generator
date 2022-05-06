9e45fe4e1df8d33f5804b3bac212474d35a8c948
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-05-06 18:05:38 +0400

    Add RuleDTO

--- src/main/java/com/energizeglobal/sqlgenerator/dto/RuleDTO.java
+++ src/main/java/com/energizeglobal/sqlgenerator/dto/RuleDTO.java
@@ -1,4 +1,41 @@
 package com.energizeglobal.sqlgenerator.dto;
 
 public class RuleDTO {
+
+    private Long id;
+    private String name;
+    private Integer orderRule;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getOrderRule() {
+        return orderRule;
+    }
+
+    public void setOrderRule(Integer orderRule) {
+        this.orderRule = orderRule;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
