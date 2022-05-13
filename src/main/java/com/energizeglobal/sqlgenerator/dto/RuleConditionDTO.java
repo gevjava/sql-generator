ce3e30f0b3a32760e12b31e724d3a2908009c1b1
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-05-13 17:26:27 +0400

    add RuleCondition dto

--- src/main/java/com/energizeglobal/sqlgenerator/dto/RuleConditionDTO.java
+++ src/main/java/com/energizeglobal/sqlgenerator/dto/RuleConditionDTO.java
@@ -23,7 +23,7 @@ public class RuleConditionDTO {
     public String getName() { return name; }
 
     public void setName(String name) { this.name = name; }
-    
+
 
     public String getCreatedBy() { return createdBy; }
 
